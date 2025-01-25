ef847716dba5198ffb7fc442776e854c23e6d18a
TheBestLoyalistYT <mataplays7751@gmail.com>
2025-01-25 16:42:44 -0400

    Version 2. Item thingy is messed up, Identifier is not allowing the (LoyalistMod.MOD_ID, name) to exist. something to do with it not being a (string, string) idk

--- src/main/java/net/thebestloyalist/somemod/LoyalistMod.java
+++ src/main/java/net/thebestloyalist/somemod/LoyalistMod.java
@@ -7,12 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LoyalistMod implements ModInitializer {
-	public static final String MOD_ID = "loyalist_mod";
+	    public static final String MOD_ID = "loyalist_mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
 		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/thebestloyalist/somemod/item/ModItems.java
+++ src/main/java/net/thebestloyalist/somemod/item/ModItems.java
@@ -1,22 +1,28 @@
 package net.thebestloyalist.somemod.item;
 
-import com.nimbusds.oauth2.sdk.id.Identifier;
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.thebestloyalist.somemod.LoyalistMod;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.thebestloyalist.somemod.LoyalistMod;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item FRIES = registerItem("fries", new Item(new Item.Settings() .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LoyalistMod.MOD_ID,"fries")))));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(LoyalistMod.MOD_ID, name), item); here is where it messed up :| after Identifier;
+        return Registry.register(Registries.ITEM, Identifier.of(LoyalistMod.MOD_ID, name), item);
     }
+
     public static void registerModItems() {
         LoyalistMod.LOGGER.info("Registering Mod Items for " + LoyalistMod.MOD_ID);
 
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries ->{
+           fabricItemGroupEntries.add(FRIES);
+        });
     }
 }
